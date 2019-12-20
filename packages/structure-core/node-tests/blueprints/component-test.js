ecd42d7a3d8e779ba29da82b97baf9c9d2a9bb3a
Matthew Irish <matthew@hashicorp.com>
2019-12-20 12:23:45 -0600

    stub cli prompt for overwriting files and add Sass file tests

--- packages/structure-core/node-tests/blueprints/component-test.js
+++ packages/structure-core/node-tests/blueprints/component-test.js
@@ -7,13 +7,8 @@ const emberGenerateDestroy = blueprintHelpers.emberGenerateDestroy;
 
 const expect = require('ember-cli-blueprint-test-helpers/chai').expect;
 
-describe('Acceptance: ember generate and destroy component', function() {
-  setupTestHooks(this);
 
-  it('component foo', function() {
-    let args = ['component', 'foo'];
-
-    let expectedComponentComment = `/**
+let expectedComponentComment = `/**
  *
  * \`St::Foo\` description here.
  *
@@ -26,16 +21,76 @@ describe('Acceptance: ember generate and destroy component', function() {
  * @class StFoo
  *
  *`;
-
-    let expectedTagNameDeclaration = `tagName: '',`;
-    let expectedTemplateStructure = `<div class="st-foo">
+let expectedTagNameDeclaration = `tagName: '',`;
+let expectedTemplateStructure = `<div class="st-foo">
   {{yield}}
-<div>`;
+</div>`;
+let expectedSassStructure = `.st-foo {
+
+}`;
+
+let td = require('testdouble');
+td.config({ promiseConstructor: require('rsvp').Promise });
+
+let MockUI = require('console-ui/mock');
+
+let test = (componentName, assertFn) => {
+  let args = ['component', componentName];
     return emberNew({target: 'addon'})
-      .then(() => emberGenerateDestroy(args, (file) => {
-         expect(file('addon/components/st/foo.js')).to.contain(expectedComponentComment);
-         expect(file('addon/components/st/foo.js')).to.contain(expectedTagNameDeclaration);
-         expect(file('addon/templates/components/st/foo.js')).to.contain(expectedTemplateStructure);
-    }));
+      .then(() => emberGenerateDestroy(args, assertFn));
+};
+describe('Acceptance: ember generate and destroy component', function() {
+  setupTestHooks(this);
+
+  let prompt;
+  beforeEach(function() {
+    prompt = td.function();
+    td.replace(MockUI.prototype, 'prompt', prompt);
+    td.when(prompt(td.matchers.anything())).thenResolve({ answer: 'overwrite', deleteFiles: 'all' });
   });
+
+  afterEach(function() {
+    td.reset();
+  });
+
+  it(`generates component class for 'foo'`, function() {
+    return test('foo', file => {
+      expect(file('addon/components/st/foo.js'))
+        .to.contain(expectedComponentComment)
+        .to.contain(expectedTagNameDeclaration);
+    });
+  });
+
+  it(`generates component class for 'st/foo'`, function() {
+    return test('st/foo', file => {
+      expect(file('addon/components/st/foo.js'))
+        .to.contain(expectedComponentComment)
+        .to.contain(expectedTagNameDeclaration);
+    });
+  });
+
+  it(`generates a template file for 'foo'`, function() {
+    return test('foo', file => {
+      expect(file('addon/templates/components/st/foo.hbs')).to.contain(expectedTemplateStructure);
+    });
+  });
+
+  it(`generates a template file for 'st/foo'`, function() {
+    return test('st/foo', file => {
+      expect(file('addon/templates/components/st/foo.hbs')).to.contain(expectedTemplateStructure);
+    });
+  });
+
+  it(`generates a Sass file for 'foo'`, function() {
+    return test('foo', file => {
+      expect(file('addon/components/st/foo.scss')).to.contain(expectedSassStructure);
+    });
+  });
+
+  it(`generates a Sass file for 'st/foo'`, function() {
+    return test('st/foo', file => {
+      expect(file('addon/components/st/foo.scss')).to.contain(expectedSassStructure);
+    });
+  });
+
 });
