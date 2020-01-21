5f9c02196e044aadb22ca4e0980d2b2a547dda9b
Matthew Irish <meirish@users.noreply.github.com>
2020-01-21 14:18:10 -0600

    Storybook setup (#2)
    * add packages and config for storybook
    
    * add story blueprint and test
    
    * update storybook so we can use addon docs auto-generation
    
    * use new more succinct main.js config for storybook - see https://github.com/storybookjs/storybook/pull/9246
    
    * add generated preview-head to gitignore
    
    * add top-level storybook:build yarn script
    
    * add storybook output dir to gitignore

---
+++ packages/structure-core/.storybook/main.js
@@ -0,0 +1,6 @@
+module.exports = {
+  addons: [
+    '@storybook/addon-docs',
+	],
+  stories: ['../stories/**/*.stories.(js|mdx)'],
+};
---
+++ packages/structure-core/.storybook/preview.js
@@ -0,0 +1,4 @@
+import { setJSONDoc } from '@storybook/addon-docs/ember';
+import json from '../dist/storybook-docgen/index.json';
+
+setJSONDoc(json);
