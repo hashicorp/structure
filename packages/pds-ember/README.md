eaabbdb9bade49aa92de1174355eae4adb893009
Ryan Johnson <rhino.citguy@gmail.com>
2020-09-30 18:22:37 -0500

    chore(pds-ember): initialize addon file structure

---
+++ packages/pds-ember/README.md
@@ -0,0 +1,33 @@
+# pds-ember
+
+Ember addon for building Structure-styled UIs.
+
+
+# Compatibility
+
+* Ember.js v3.16 or above
+* Ember CLI v2.13 or above
+* Node.js v10 or above
+
+
+# Installation
+
+```
+ember install @hashicorp/pds-ember
+```
+
+
+# Usage
+
+[Longer description of how to use the addon in apps.]
+
+
+# Contributing
+
+See the [Contributing](CONTRIBUTING.md) guide for details.
+
+
+# License
+------------------------------------------------------------------------------
+
+This project is licensed under the [MPLv2.0 License](LICENSE.txt).
--- packages/pds-ember/index.js
+++ packages/pds-ember/index.js
@@ -1,1 +1,32 @@
-module.exports = 'Hello, from @hashicorp/pds-ember'
+'use strict';
+
+let Funnel = require('broccoli-funnel');
+let mergeTrees = require('broccoli-merge-trees');
+let path = require('path');
+
+module.exports = {
+  name: require('./package').name,
+
+  isDevelopingAddon() {
+    return true;
+  },
+
+  // (pulled from HCP:packages/addon-product-design-system)
+  treeForStyles: function() {
+    let stylesPath = path.join(__dirname, 'app/styles/pds');
+
+    let PDS = new Funnel(stylesPath, {
+      destDir: 'pds',
+      annotation: 'PDS Styles',
+    });
+    let mergedTrees = mergeTrees([ PDS ], { overwrite: true });
+
+    return this._super.treeForStyles(mergedTrees);
+  },
+
+  // required for ember-cli-sass (according to docs)
+  included: function(/* app */) {
+    this._super.included.apply(this, arguments);
+  },
+};
+
