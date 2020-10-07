316e1da686ef39007391df5067852ab2237c8654
Ryan Johnson <rhino.citguy@gmail.com>
2020-10-06 19:06:32 -0500

    feat!(pds-ember): add Link styles
    BREAKING:
    `pds/core` (Sass) no longer includes core Link styles.
    Use `pds/components/link` instead.

---
+++ packages/pds-ember/addon/components/pds/link/stories/index.stories.js
@@ -0,0 +1,78 @@
+import hbs from 'htmlbars-inline-precompile';
+import DocsPage, { TITLE } from '../docs.mdx';
+
+const CONFIG = {
+  title: TITLE,
+  parameters: { docs: { page: DocsPage } },
+};
+
+const Index = () => ({
+  template: hbs`
+    <a href="#">
+      Read More
+    </a>
+  `,
+});
+
+const Hover = () => ({
+  template: hbs`
+    <a href="#" class="mock-hover">
+      Read More
+    </a>
+  `,
+});
+
+const Focus = () => ({
+  template: hbs`
+    <a href="#" class="mock-focus">
+      Read More
+    </a>
+  `,
+});
+
+const FocusHover = () => ({
+  template: hbs`
+    <a href="#" class="mock-focus mock-hover">
+      Read More
+    </a>
+  `,
+});
+
+const Active = () => ({
+  template: hbs`
+    <a href="#" class="mock-active">
+      Read More
+    </a>
+  `,
+});
+
+const WithLeadingIcon = () => ({
+  template: hbs`
+    <a href="#">
+      <Pds::Icon @type="info-circle-fill" />
+      Read More
+    </a>
+  `
+});
+
+const WithTrailingIcon = () => ({
+  template: hbs`
+    <a href="#">
+      Read More
+      <Pds::Icon @type="exit" />
+    </a>
+  `
+});
+
+export {
+  CONFIG as default,
+  Index,
+
+  Hover,
+  Focus,
+  FocusHover,
+  Active,
+
+  WithLeadingIcon,
+  WithTrailingIcon,
+}
