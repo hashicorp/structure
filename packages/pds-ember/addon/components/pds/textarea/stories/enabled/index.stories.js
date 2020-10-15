ea60abca64d6c2bd9b000373a6f51fc25db15a0f
Ryan Johnson <rhino.citguy@gmail.com>
2020-10-15 18:08:09 -0500

    feat(pds-ember): add Textarea component

---
+++ packages/pds-ember/addon/components/pds/textarea/stories/enabled/index.stories.js
@@ -0,0 +1,41 @@
+import hbs from 'htmlbars-inline-precompile';
+import Parent from '../index.stories.js';
+
+export default {
+  ...Parent,
+  title: `${Parent.title} / Enabled`,
+  parameters: {
+    controls: { disable: true }
+  },
+}
+
+export const Blurred = () => ({
+  template: hbs`
+    <Pds::Textarea>Enabled (blur)</Pds::Textarea>
+  `,
+});
+
+export const Hover = () => ({
+  template: hbs`
+    <Pds::Textarea
+      class="mock-hover"
+    >Enabled (hover)</Pds::Textarea>
+  `,
+});
+
+export const Focus = () => ({
+  template: hbs`
+    <Pds::Textarea
+      class="mock-focus"
+    >Enabled (focus)</Pds::Textarea>
+  `,
+});
+
+export const FocusHover = () => ({
+  template: hbs`
+    <Pds::Textarea
+      class="mock-focus mock-hover"
+    >Enabled (focus + hover)</Pds::Textarea>
+  `,
+});
+FocusHover.storyName = 'Focus + Hover';
---
+++ packages/pds-ember/addon/components/pds/textarea/stories/enabled/invalid.stories.js
@@ -0,0 +1,43 @@
+import hbs from 'htmlbars-inline-precompile';
+import Parent from './index.stories.js';
+
+export default {
+  ...Parent,
+  title: `${Parent.title} / Invalid`,
+}
+
+export const Blurred = () => ({
+  template: hbs`
+    <Pds::Textarea
+      @invalid={{true}}
+    >Invalid (blur)</Pds::Textarea>
+  `
+});
+
+export const Hover = () => ({
+  template: hbs`
+    <Pds::Textarea
+      class="mock-hover"
+      @invalid={{true}}
+    >Invalid (hover)</Pds::Textarea>
+  `
+});
+
+export const Focus = () => ({
+  template: hbs`
+    <Pds::Textarea
+      class="mock-focus"
+      @invalid={{true}}
+    >Invalid (focus)</Pds::Textarea>
+  `
+});
+
+export const FocusHover = () => ({
+  template: hbs`
+    <Pds::Textarea
+      class="mock-focus mock-hover"
+      @invalid={{true}}
+    >Invalid (focus + hover)</Pds::Textarea>
+  `
+});
+FocusHover.storyName = 'Focus + Hover';
