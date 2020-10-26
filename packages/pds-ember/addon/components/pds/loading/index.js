f982c873eabf139389f684ef11fd56b015617ec7
Ryan Johnson <rhino.citguy@gmail.com>
2020-10-26 15:22:22 -0500

    feat(pds-ember): add Loading component
    Port `<Loading>` and its subcomponents from HCP, with the following differences:
    
    1. some contextual styles previously present in HCP are not included
      - `.pdsAppContent > .fullHeight .loadingState`
        - (no context to know if it's necessary)
    
    JIRA:
      * PDS-161
      * PDS-162
      * PDS-163

---
+++ packages/pds-ember/addon/components/pds/loading/index.js
@@ -0,0 +1,6 @@
+import Component from '@glimmer/component'
+
+/**
+ * @class PdsLoading
+ */
+export default class PdsLoading extends Component {}
---
+++ packages/pds-ember/addon/components/pds/loading/message/index.js
@@ -0,0 +1,6 @@
+import Component from '@glimmer/component'
+
+/**
+ * @class PdsLoadingMessage
+ */
+export default class PdsLoadingMessage extends Component {}
