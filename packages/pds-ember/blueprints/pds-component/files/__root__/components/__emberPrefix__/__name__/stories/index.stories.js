687a540a97047245934ffa1315adf9297aebcfad
Ryan Johnson <rhino.citguy@gmail.com>
2020-10-28 12:56:21 -0500

    dev(pds-ember): update blueprints

--- packages/pds-ember/blueprints/pds-component/files/__root__/components/__emberPrefix__/__name__/stories/index.stories.js
+++ packages/pds-ember/blueprints/pds-component/files/__root__/components/__emberPrefix__/__name__/stories/index.stories.js
@@ -1,20 +1,22 @@
-import hbs from 'htmlbars-inline-precompile';
-import { withKnobs } from '@storybook/addon-knobs';
-import DocsPage, { TITLE } from '../docs.mdx';
+import hbs from 'htmlbars-inline-precompile'
+import DocsPage, { TITLE } from '../docs.mdx'
 
-const CONFIG = {
+export default {
   title: TITLE,
   component: '<%= jsClass %>',
-  decorators: [ withKnobs ],
   parameters: { docs: { page: DocsPage } },
-};
+}
 
-const Index = () => ({
-  template: hbs`<<%= tagName %> />`,
-});
-
-export {
-  CONFIG as default,
-
-  Index,
-};
+export const Index = (args) => ({
+  template: hbs`
+    <<%= tagName %>
+      @isAwesome={{isAwesome}}
+    >
+      block content
+    </<%= tagName %>>
+  `,
+  context: args,
+})
+Index.args = {
+  isAwesome: true,
+}
