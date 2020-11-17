f5f010d6c991686b7dc57d8658459a9e15876ed3
Ryan Johnson <rhino.citguy@gmail.com>
2020-11-16 20:17:27 -0600

    feat(pds-ember): add Pds::Dropdown
    - add `<Pds::Dropdown>` (replacement for HCP `<Menu>`)
    - add `<Pds::Dropdown::Trigger>` (replacement for HCP `<Menu::Trigger>`)
    - add `<Pds::Dropdown::Dialog>` (replacement for HCP `<Menu::Content>`)
    - add missing tests for `<Pds::Button>` component (discovered as I was
      writing tests for `<Pds::Dropdown::Trigger>`)
    - remove `summary.pds-button` from Button > States (storybook)
    
    JIRA: PDS-191

---
+++ packages/pds-ember/addon/components/pds/dropdown/dialog/index.js
@@ -0,0 +1,6 @@
+import Component from '@glimmer/component'
+
+/**
+ * @class PdsDropdownDialog
+ */
+export default class PdsDropdown extends Component {}
--- packages/pds-ember/tests/fixtures/components/button-fixtures.js
+++ packages/pds-ember/tests/fixtures/components/button-fixtures.js
@@ -1,16 +1,20 @@
-export const VARIANT_CLASSES = {
-  ghost: 'pds--ghost',
-  primary: 'pds--primary',
-  secondary: '',
-  warning: 'pds--warning',
-  // non-existent variant
-  nonesuch: '',
-}
-
 export const CSS = {
   Button: 'pds-button',
+  compact: 'pds--compact',
   iconEnd: 'pds--iconEnd',
   iconOnly: 'pds--iconOnly',
   iconStart: 'pds--iconStart',
   visuallyHidden: 'pds--visuallyHidden',
+  primary: 'pds--primary',
+  ghost: 'pds--ghost',
+  warning: 'pds--warning',
+}
+
+export const VARIANT_CLASSES = {
+  ghost: CSS.ghost,
+  primary: CSS.primary,
+  secondary: '',
+  warning: CSS.warning,
+  // non-existent variant
+  nonesuch: '',
 }
