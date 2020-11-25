18667a5e692113a32ad1f41a090beb7bcd448749
Ryan Johnson <rhino.citguy@gmail.com>
2020-11-25 14:17:50 -0600

    feat(pds-ember): add Pds::Modal
    JIRA: PDS-138

---
+++ packages/pds-ember/addon/components/pds/modal/utils.js
@@ -0,0 +1,9 @@
+export const DEFAULT_VARIANT = 'plain'
+
+export const ICON_TYPES = {
+  'plain': 'alert-circle-fill',
+  'error': 'cancel-square-fill',
+  'info': 'info-circle-fill',
+  'success': 'check-circle-fill',
+  'warning': 'alert-triangle',
+}
