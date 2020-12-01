008cf9a02df9ab74e6e38fe0816cc98292692a6f
Ryan Johnson <rhino.citguy@gmail.com>
2020-11-30 18:59:57 -0600

    feat(pds-ember): add Divider component

---
+++ packages/pds-ember/addon/components/pds/divider/stories/index.stories.js
@@ -0,0 +1,22 @@
+import hbs from 'htmlbars-inline-precompile'
+import DocsPage, { TITLE } from '../docs.mdx'
+
+export default {
+  title: TITLE,
+  component: 'PdsDivider',
+  parameters: { docs: { page: DocsPage } },
+  args: {
+    vertical: false,
+  },
+}
+
+export const Index = (args) => ({
+  template: hbs`
+    <p>
+      <span>BEFORE</span>
+      <Pds::Divider @vertical={{vertical}} />
+      <span>AFTER</span>
+    </p>
+  `,
+  context: args,
+})
