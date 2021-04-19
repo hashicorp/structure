3a4fc1f20c4fdfb155d2ab31d7defc990d0beac1
mo cohen <mo.cohen@hashicorp.com>
2021-04-19 14:42:33 -0700

    Adding bold variant to help text

---
+++ packages/pds-ember/addon/components/pds/help-text/index.js
@@ -0,0 +1,28 @@
+import Component from '@glimmer/component';
+import { getVariantClass } from './utils';
+
+/**
+ * @class HelpText
+ */
+
+/**
+ * Display variant.
+ *
+ * _(default: `''`)_
+ *
+ * If left blank or not defined, defaults to "secondary" button appearance.
+ *
+ * Valid values include:
+ *
+ * - `bold`
+ *
+ * @argument variant
+ * @type { string }
+ * @default ''
+ */
+
+export default class extends Component {
+    get variantClass() {
+        return getVariantClass(this.args.variant);
+    }
+}
---
+++ packages/pds-ember/addon/components/pds/help-text/utils.js
@@ -0,0 +1,17 @@
+export const VARIANT_CLASSES = {
+    bold: 'pds-helpText--bold',
+    regular: '',
+  };
+  
+export const DEFAULT_VARIANT = 'regular';
+
+export function getVariantClass(variant) {
+let result = '';
+
+if (VARIANT_CLASSES[variant]) {
+    result = VARIANT_CLASSES[variant];
+}
+
+return result;
+}
+  
